1d77392cd805d8c6a3d73ec72fcb2eae595034d6
Sabbir Nasir <18-36044-1@student.aiub.edu>
2022-08-08 11:42:45 +0600

    Exception Handling

---
+++ exception_handle/ExpectionHandleDemo.java
@@ -0,0 +1,32 @@
+package exception_handle;
+
+//import java.util.*;
+import java.io.*;
+
+public class ExpectionHandleDemo {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+		// runtime expections = checked exception
+
+		// int data = 100/0;
+
+		// System.out.println(data);
+//		
+//		String a = null;
+//		System.out.println(a.charAt(0));
+
+		// unchecked exceptions
+
+//		FileReader file = new FileReader("C:\\test\\a.txt");
+//		BufferedReader fileInput = new BufferedReader(file);
+//
+//		for (int counter = 0; counter < 3; counter++) {
+//			System.out.println(fileInput.readLine());
+//		}
+//		fileInput.close();
+
+	}
+
+}
